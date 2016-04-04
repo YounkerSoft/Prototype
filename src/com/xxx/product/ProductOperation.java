3ac9ab4fbed2c2051c18a4dc0c0d28ed8e1cfad7
yuanzichao <yuanzichao90@126.com>
2016-04-04 15:18:10 +0800

    产品操作工具类：从产品库中获取产品的基本信息

---
+++ src/com/xxx/product/ProductOperation.java
@@ -0,0 +1,26 @@
+package com.xxx.product;
+
+import com.xxx.product.bean.SKUProduct;
+
+/**
+ * @author yuan
+ *
+ * @date 2016年4月4日 下午2:15:53
+ */
+public class ProductOperation {
+	
+	//从产品库中获取信息
+	public static SKUProduct getSKUProduct(String sku){
+		SKUProduct SKUPro = null;
+		
+		/******************************************/
+		/***************产品库构建未实现****************/
+		/******************************************/
+		//根据sku值从产品库中获取该产品的生产参数
+		//产品库可能是数据库，也可能是文本（待定）
+		
+		
+		return SKUPro;
+	}
+
+}
