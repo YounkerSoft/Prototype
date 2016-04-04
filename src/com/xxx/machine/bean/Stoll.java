6dda53a317b1555767e86d39fa9fb869613a3c90
yuanzichao <yuanzichao90@126.com>
2016-04-04 19:48:06 +0800

    新增：已完成任务队列

--- src/com/xxx/machine/bean/Stoll.java
+++ src/com/xxx/machine/bean/Stoll.java
@@ -16,18 +16,20 @@ public class Stoll extends Machine{
 	
 	private String modelType;			//机器型号
 	
-	private ArrayList<SPUProduct> productList;			//产品列表
+	private ArrayList<SPUProduct> productList;			//所能生产产品列表
 	
 	private String location;			//机器所处位置（是否处于一个市县乡）
 	private String factory;				//机器所在工厂
 	
 	private int status;					//机器当前状态（0：空闲；1：运转；2：故障）
 	private ArrayList<ProductionOrder> taskQueue;		//任务队列
+	private ArrayList<ProductionOrder> comTaskQueue;	//已完成任务队列
 	private ArrayList<ProductionOrder> curTaskQueue;	//当前任务队列
 	private int estimatedIdleTime;						//完成当前任务队列中的任务预计所需时间
 	private String history;				//历史||日志
 	
-	
+
+
 	/**
 	 * @param machineId
 	 * @param modelType
@@ -36,6 +38,7 @@ public class Stoll extends Machine{
 	 * @param factory
 	 * @param status
 	 * @param taskQueue
+	 * @param comTaskQueue
 	 * @param curTaskQueue
 	 * @param estimatedIdleTime
 	 * @param history
@@ -43,6 +46,7 @@ public class Stoll extends Machine{
 	public Stoll(long machineId, String modelType,
 			ArrayList<SPUProduct> productList, String location, String factory,
 			int status, ArrayList<ProductionOrder> taskQueue,
+			ArrayList<ProductionOrder> comTaskQueue,
 			ArrayList<ProductionOrder> curTaskQueue, int estimatedIdleTime,
 			String history) {
 		super();
@@ -53,6 +57,7 @@ public class Stoll extends Machine{
 		this.factory = factory;
 		this.status = status;
 		this.taskQueue = taskQueue;
+		this.comTaskQueue = comTaskQueue;
 		this.curTaskQueue = curTaskQueue;
 		this.estimatedIdleTime = estimatedIdleTime;
 		this.history = history;
@@ -195,6 +200,22 @@ public class Stoll extends Machine{
 	 */
 	public void setTaskQueue(ArrayList<ProductionOrder> taskQueue) {
 		this.taskQueue = taskQueue;
+	}
+
+
+	/**
+	 * @return the comTaskQueue
+	 */
+	public ArrayList<ProductionOrder> getComTaskQueue() {
+		return comTaskQueue;
+	}
+
+
+	/**
+	 * @param comTaskQueue the comTaskQueue to set
+	 */
+	public void setComTaskQueue(ArrayList<ProductionOrder> comTaskQueue) {
+		this.comTaskQueue = comTaskQueue;
 	}
 
 
