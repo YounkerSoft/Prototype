f259425c0e9e54561036b589a99e3e5820d57413
yuanzichao <yuanzichao90@126.com>
2016-04-07 21:23:54 +0800

    实现：getSKUProduct(String sku)//从产品库中获取产品信息

--- src/com/xxx/product/ProductOperation.java
+++ src/com/xxx/product/ProductOperation.java
@@ -1,6 +1,14 @@
 package com.xxx.product;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+
 import com.xxx.product.bean.SKUProduct;
+import com.xxx.util.DTOPopulator;
+import com.xxx.util.Database;
 
 /**
  * @author yuan
@@ -9,16 +17,35 @@ import com.xxx.product.bean.SKUProduct;
  */
 public class ProductOperation {
 	
+	//SQL connection
+	static Connection connection = null;
+		
+	
 	//从产品库中获取信息
-	public static SKUProduct getSKUProduct(String sku){
+	@SuppressWarnings("unchecked")
+	public static SKUProduct getSKUProduct(String sku) throws Exception{
 		SKUProduct SKUPro = null;
 		
 		/******************************************/
-		/***************产品库构建未实现****************/
+		/***********产品库构建实现（采用MySql）***********/
 		/******************************************/
-		//根据sku值从产品库中获取该产品的生产参数
-		//产品库可能是数据库，也可能是文本（待定）
 		
+		//========>>未分割<<========
+		
+		connection = Database.getConnection();
+		String sql = "select * from sku where sku=" + sku;
+		PreparedStatement ps = connection.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		List<SKUProduct> list = DTOPopulator.populate(rs, SKUProduct.class);
+		
+		if(list.size()!=0 && list!=null){
+			SKUPro = list.get(0);
+		} else {
+			System.out.println("产品库中无该产品信息！");
+		}
+		
+		rs.close();
+		ps.close();	
 		
 		return SKUPro;
 	}
